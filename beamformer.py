c67ea5f862eb33da23a404b77e34d99e55424361
arcane <12jspeake@gmail.com>
2024-02-26 21:36:20 +0000

    Initial working simulator and beamformer

--- beamformer.py
+++ beamformer.py
@@ -1,26 +1,103 @@
 import numpy as np
 
+def calculate_array_factor(phases, antenna, parameters):
+    array_factor = 20 * np.log10(np.abs(calculate_er(phases, antenna, parameters)))
+    return array_factor
+
+def calculate_er(phases, antenna, parameters):
+    # TODO - hot code
+    e_r = np.zeros(parameters["samples"], dtype=np.csingle)
+    phi = parameters["phi"]
+    for i in range(parameters["samples"]):
+        angle = phi[i]
+        theta = antenna["wavenumber"] * np.cos(angle) * antenna["positions"] + phases
+        e_t = np.sum(antenna["weights"] * np.exp(1j * theta))
+        e_r[i] = e_t/antenna["num_elements"]
+    return e_r
+
+def new_population(antenna, parameters):
+    population = []
+    for i in range(parameters["population_size"]):
+        population.append(new_particle(antenna, parameters))
+    return population
+
+def fitness(position, antenna, parameters):
+    array_factor = calculate_array_factor(position, antenna, parameters)
+    score = 0
+    for target in parameters["target_samples"]:
+        score = score + array_factor[target]
+    return score
+
+def new_particle(antenna, parameters):
+    position = np.random.rand(antenna["num_elements"]) *2*np.pi
+    score = fitness(position, antenna, parameters)
+    return {
+        "best_position" : position,
+        "position" : position,
+        "velocity" : np.random.rand(antenna["num_elements"]),
+        "score" : score
+    }
+
+def define_parameters(pop_size, angular_samples, cognitive_coeff, social_coeff, intertia_weight, max_steps, static_targets):
+    phi = np.linspace(0, np.pi, angular_samples)
+    target_samples = np.asarray(static_targets) * angular_samples / np.pi
+    target_samples = target_samples.astype(int)
+    return {
+        "population_size" : pop_size,
+        "samples" : angular_samples,
+        "phi" : phi,
+        "cognitive_coeff" : cognitive_coeff,
+        "social_coeff" : social_coeff,
+        "intertia_weight" : intertia_weight,
+        "max_steps" : max_steps,
+        "target_samples" : target_samples,
+    }
+
 def define_ULA(frequency, spacing_coeff, num_elements):
     wavelength = 3e9/frequency
     spacing = wavelength * spacing_coeff
     N = (num_elements - 1)/2
     positions = np.linspace(spacing*-N, spacing*N, num_elements)
     return {
-        "elements" : num_elements,
+        "num_elements" : num_elements,
         "spacing" : spacing,
         "positions" : positions,
         "frequency" : frequency,
         "wavelength" : wavelength,
         "wavenumber" : 2*np.pi / wavelength,
+        "weights" : np.ones(num_elements)
     }
 
-def define_parameters(samples, pop_size):
-    return {
-        "samples" : samples,
-        "population_size" : pop_size,
-    }
+def particle_swarm_optimisation(antenna, parameters):
+    population = new_population(antenna, parameters)
+    best_known_position = np.random.rand(antenna["num_elements"]) *2*np.pi
+    best_score = -1000
+    step = 0
+    while step <= parameters["max_steps"]:
+        print(f"Step: {step}")
+        population, best_known_position, best_score = step_PSO(population, best_known_position, best_score, antenna, parameters)
+        step += 1
+        print(f"Position: {best_known_position}\n Score: {best_score}")
+    return best_known_position
+
+def step_PSO(population, best_known_position, best_score, antenna, parameters):
+    for particle in population:
+        a = parameters["intertia_weight"]*particle["velocity"]
+        b = parameters["cognitive_coeff"]*np.random.rand(antenna["num_elements"])*(np.subtract(particle["best_position"], particle["position"]))
+        c = parameters["social_coeff"]*np.random.rand(antenna["num_elements"])*(np.subtract(best_known_position, particle["position"]))
+        particle["velocity"] = a + b + c # TODO refactor readability
+        particle["position"] = np.add(particle["position"], particle["velocity"])
+        score = fitness(particle["position"], antenna, parameters)
+        if score > best_score:
+            best_known_position = particle["position"]
+            best_score = score
+            particle["best_position"] = particle["position"]
+            particle["score"] = score
+        elif score > particle["score"]:
+            particle["best_position"] = particle["position"]
+            particle["score"] = score
+    return population, best_known_position, best_score
 
 def beamformer(antenna, parameters):
-    feedback_antenna = f"Antenna: {antenna}"
-    feedback_parameters = f"Parameters: {parameters}"
-    return f"{feedback_antenna} \n {feedback_parameters}"
+    result = particle_swarm_optimisation(antenna, parameters)
+    return result
--- simulator.py
+++ simulator.py
@@ -2,8 +2,13 @@ import beamformer as bf
 
 def main():
     antenna = bf.define_ULA(2e9, 0.5, 10)
-    parameters = bf.define_parameters(1800, 25)
+    static_targets = [0.5, 1.2] # in radians
+    population_size = 100
+    angular_samples = 1500
+    intertia_weight = 0.5
+    cognitive_coeff = 0.8
+    social_coeff = 0.8
+    parameters = bf.define_parameters(population_size, angular_samples, cognitive_coeff, social_coeff, intertia_weight, 25, static_targets)
     result = bf.beamformer(antenna, parameters)
-    print(result)
 
 main()
