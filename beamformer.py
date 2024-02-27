76b7ea58f1573129c95719a2f85cb4645f35341f
arcane <12jspeake@gmail.com>
2024-02-27 01:23:47 +0000

    Add toggling display in simulation

--- beamformer.py
+++ beamformer.py
@@ -25,8 +25,8 @@ def new_population(antenna, parameters):
 def fitness(position, antenna, parameters):
     array_factor = calculate_array_factor(position, antenna, parameters)
     score = 0
-    for target in parameters["target_samples"]:
-        score += array_factor[target]
+    for target in parameters["targets"]:
+        score = score + array_factor[target]
         # score += (array_factor[target-1] + array_factor[target] + array_factor[target+1])/3
     return score
 
@@ -40,22 +40,26 @@ def new_particle(antenna, parameters):
         "score" : score
     }
 
-def display(position, antenna, parameters):
+def display(position, antenna, parameters, persist=False):
     array_factor = calculate_array_factor(position, antenna, parameters)
     plt.clf()
     plt.plot(parameters["phi"]-np.pi/2, array_factor)
-    for target in parameters["target_samples"]:
-        plt.axvline(2*target/parameters["samples"], color="red")
+    targets_markers = 2*np.pi * parameters["targets"] / parameters["samples"]
+    for target in targets_markers:
+        plt.axvline(target, color="red")
     plt.xlim(-np.pi/2, np.pi/2)
     plt.ylim((-50, 0))
     plt.xlabel('Beam angle [rad]')
     plt.ylabel('Power [dB]')
-    plt.pause(0.01)
+    if persist:
+        plt.show()
+    else:
+        plt.pause(0.05)
 
 def define_parameters(pop_size, angular_samples, cognitive_coeff, social_coeff, intertia_weight, max_steps, static_targets):
     phi = np.linspace(0, np.pi, angular_samples)
-    target_samples = np.asarray(static_targets) * angular_samples / np.pi
-    target_samples = target_samples.astype(int)
+    targets = np.asarray(static_targets) * angular_samples / (2*np.pi)
+    targets = targets.astype(int)
     return {
         "population_size" : pop_size,
         "samples" : angular_samples,
@@ -64,7 +68,13 @@ def define_parameters(pop_size, angular_samples, cognitive_coeff, social_coeff, 
         "social_coeff" : social_coeff,
         "intertia_weight" : intertia_weight,
         "max_steps" : max_steps,
-        "target_samples" : target_samples,
+        "targets" : targets,
+    }
+
+def define_logging(show_plots, plots_persist):
+    return {
+        "show_plots" : show_plots,
+        "plots_persist" : plots_persist,
     }
 
 def define_ULA(frequency, spacing_coeff, num_elements):
@@ -82,7 +92,7 @@ def define_ULA(frequency, spacing_coeff, num_elements):
         "weights" : np.ones(num_elements)
     }
 
-def particle_swarm_optimisation(antenna, parameters):
+def particle_swarm_optimisation(antenna, parameters, logging):
     population = new_population(antenna, parameters)
     best_known_position = np.random.rand(antenna["num_elements"]) *2*np.pi
     best_score = -1000
@@ -92,7 +102,7 @@ def particle_swarm_optimisation(antenna, parameters):
         population, best_known_position, best_score = step_PSO(population, best_known_position, best_score, antenna, parameters)
         step += 1
         print(f"Position: {best_known_position}\n Score: {best_score}")
-        display(best_known_position, antenna, parameters)
+        if logging["show_plots"]: display(best_known_position, antenna, parameters)
     return best_known_position
 
 def step_PSO(population, best_known_position, best_score, antenna, parameters):
@@ -113,7 +123,7 @@ def step_PSO(population, best_known_position, best_score, antenna, parameters):
             particle["score"] = score
     return population, best_known_position, best_score
 
-def beamformer(antenna, parameters):
-    best_found_position = particle_swarm_optimisation(antenna, parameters)
-    display(best_found_position, antenna, parameters)
+def beamformer(antenna, parameters, logging):
+    result = particle_swarm_optimisation(antenna, parameters, logging)
+    if logging["show_plots"] and logging["plots_persist"]: display(result, antenna, parameters, persist=True)
     return result
--- simulator.py
+++ simulator.py
@@ -1,14 +1,16 @@
 import beamformer as bf
 
 def main():
-    antenna = bf.define_ULA(2e9, 0.5, 10)
+    antenna = bf.define_ULA(2e9, 0.5, 15)
     static_targets = [1, -1] # in radians
     population_size = 100
-    angular_samples = 1500
-    intertia_weight = 0.5
-    cognitive_coeff = 0.8
-    social_coeff = 0.8
-    parameters = bf.define_parameters(population_size, angular_samples, cognitive_coeff, social_coeff, intertia_weight, 25, static_targets)
-    result = bf.beamformer(antenna, parameters)
+    angular_samples = 1000
+    intertia_weight = 0.9
+    cognitive_coeff = 0.5
+    social_coeff = 0.5
+    max_steps = 50
+    parameters = bf.define_parameters(population_size, angular_samples, cognitive_coeff, social_coeff, intertia_weight, max_steps, static_targets)
+    logging = bf.define_logging(show_plots=True, plots_persist=True)
+    result = bf.beamformer(antenna, parameters, logging)
 
 main()
