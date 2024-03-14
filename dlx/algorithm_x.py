c2c297b04410763e19f6acf378337017704864a7
z-arcane <sspeake@protonmail.com>
2024-03-14 21:54:13 +0000

    Get DLX working?

--- dlx/algorithm_x.py
+++ dlx/algorithm_x.py
@@ -1,7 +1,14 @@
+from cgitb import small
+from re import purge
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 import copy
+
+from pyparsing import col
+
+"""An implementation of Donald Knuth's Dancing Links Algorithm X"""
+"""https://arxiv.org/pdf/cs/0011047.pdf"""
 
 class Data:
     def __init__(self, up, down, header, left=None, right=None):
@@ -69,11 +76,9 @@ class DLL_2D:
     def find_smallest_header(self):
         """Returns the column header with the smallest number of 1s in it's column"""
         selected = self.main_header.right
-        smallest_size = selected.size
         smallest_header = self.main_header.right
         while selected.name is not None:
-            if selected.size < smallest_size:
-                smallest_size = selected.size
+            if selected.size < smallest_header.size:
                 smallest_header = selected
             selected = selected.right
         return smallest_header
@@ -86,22 +91,90 @@ class DLL_2D:
             data = data.down
         return rows
 
+    def get_columns_in_row(self, row_element):
+        columns = []
+        data = row_element.right
+        while data is not row_element:
+            columns.append(data)
+            data = data.right
+        return columns
+
     def row_bits_from_element(self, element):
         """Returns the value of a row, that the given element is a member of, in a form suitable for np arrays"""
-        indices = []
-        starting_header = element.header
-        indices.append(starting_header.name)
-        element = element.right
-        column_header = element.header
-        while column_header.name != starting_header.name:
-            indices.append(column_header.name)
-            element = element.right
-            column_header = element.header
+        indices = [element.header.name]
+        data = element.right
+        while data is not element:
+            indices.append(data.header.name)
+            data = data.right
         out = np.zeros(self.solution_dimensions[1])
         out[indices] = 1
         return out
 
+
+    def cover_column(self, column):
+        header = column.header
+        header.right.left = header.left
+        header.left.right = header.right
+        i = header.down
+        while i is not header:
+            j = i.right
+            while j is not i:
+                j.down.up = j.up
+                j.up.down = j.down
+                j.header.size -= 1
+                j = j.right
+            i = i.down
+
+    def uncover_column(self, column):
+        header = column.header
+        i = header.up
+        while i is not header:
+            j = i.left
+            while j is not i:
+                j.header.size += 1
+                j.down.up = j
+                j.up.down = j
+                j = j.left
+            i = i.up
+        header.right.left = header
+        header.left.right = header
+
+
+def search(dll_2d, solution, k):
+    if dll_2d.main_header.right is dll_2d.main_header:
+        return solution
+    c = dll_2d.find_smallest_header()
+    dll_2d.cover_column(c)
+    r = c.down
+    while r is not c:
+        o_k = r
+        if len(solution) <= k:
+            solution.append(dll_2d.row_bits_from_element(r))
+        else:
+            solution[k] = dll_2d.row_bits_from_element(r)
+        j = r.right
+        while j is not r:
+            dll_2d.cover_column(j)
+            j = j.right
+        solution = search(dll_2d, solution, k+1)
+        # Lines below added in for early termination
+        # - - -
+        if dll_2d.main_header.right is dll_2d.main_header:
+            return solution
+        # - - -
+        c = r.header
+        r = o_k
+        j = r.left
+        while j is not r:
+            dll_2d.uncover_column(j)
+            j = j.left
+        r = r.down
+    dll_2d.uncover_column(c)
+    return solution
+
+
 def generate_starting_matrix(num_elements, polyomino_size):
+    """Generate a matrix representing the exact cover problem of a given array and polyomino size"""
     result = []
     for bits in itertools.combinations(range(num_elements), polyomino_size):
         row = np.zeros(num_elements, dtype=int)
@@ -109,23 +182,10 @@ def generate_starting_matrix(num_elements, polyomino_size):
         result.append(row)
     return np.asarray(result)
 
-def dancing_links_x(dll_2d, solution):
-    if dll_2d.main_header.right == dll_2d.main_header:
-        return solution
-    else:
-        next_dll_2d = copy.deepcopy(dll_2d)
-        selected_column = next_dll_2d.find_smallest_header()
-        rows_in_column = next_dll_2d.get_rows_in_column(selected_column)
-        selected_row = rows_in_column[np.random.randint(0, len(rows_in_column))]
-        solution.append(dll_2d.row_bits_from_element(selected_row))
-        # pick one at random, add it to the partial solution
-        # remove all columns satisfied by that row
-        # recurse
-
 
 def main():
-    array = np.zeros((3, 3), dtype=int)
-    polyomino_size = 3
+    array = np.zeros((4, 4), dtype=int)
+    polyomino_size = 8
     assert np.mod(array.size, polyomino_size) == 0
     print(f"Creating solution matrix for:\n    Array: {array.shape}\n    Polyomino: {polyomino_size}")
     solution_matrix = generate_starting_matrix(array.size, polyomino_size)
@@ -133,7 +193,8 @@ def main():
     dll_2d = DLL_2D(solution_matrix)
     print("Solving...")
     solution = []
-    solution = dancing_links_x(dll_2d, solution)
+    solution = search(dll_2d, solution, k=0)
+    print(f"Solution: {solution}")
 
 
 main()
