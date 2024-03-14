649bf4d99dea0cd81a303da944700594a9a88787
arcane <12jspeake@gmail.com>
2024-03-14 15:37:29 +0000

    dlx progress- wip

--- dlx/algorithm_x.py
+++ dlx/algorithm_x.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+import copy
 
 class Data:
     def __init__(self, up, down, header, left=None, right=None):
@@ -20,30 +21,18 @@ class ColumnHeader(Data):
         self.size = size
         self.name = name
 
-
 class MainHeader:
     def __init__(self):
         self.left = self
         self.right = self
         self.name = None
 
-    def find_header(self, name):
-        """Iterates through all connected column headers, returning the header with the given numerical name"""
-        selected = self.right
-        while selected.name is not None:
-            print(selected.name)
-            if selected.name == name:
-                return selected
-            selected = selected.right
-        return None
-
-
 class DLL_2D:
     def __init__(self, solution_matrix):
         self.main_header = MainHeader()
+        self.solution_dimensions = solution_matrix.shape
         # Insert all column headers
         for c, column in enumerate(solution_matrix.T):
-            print(f"C: {c}")
             ones_in_column = np.sum(column)
             header = ColumnHeader(left=self.main_header.left, right=self.main_header, size=ones_in_column, name=c)
             self.main_header.left.right = header
@@ -61,12 +50,56 @@ class DLL_2D:
             for i, data in enumerate(data_points):
                 data.left = data_points[i-1]
                 data.right = data_points[np.mod(i+1, len(data_points))]
-                header = self.main_header.find_header(column_indices[i])
+                header = self.find_header(column_indices[i])
                 data.header = header
                 data.up = header.up
                 data.down = header
                 header.up.down = data
                 header.up = data
+
+    def find_header(self, name):
+        """Iterates through all connected column headers, returning the header with the given numerical name"""
+        selected = self.main_header.right
+        while selected.name is not None:
+            if selected.name == name:
+                return selected
+            selected = selected.right
+        return None
+
+    def find_smallest_header(self):
+        """Returns the column header with the smallest number of 1s in it's column"""
+        selected = self.main_header.right
+        smallest_size = selected.size
+        smallest_header = self.main_header.right
+        while selected.name is not None:
+            if selected.size < smallest_size:
+                smallest_size = selected.size
+                smallest_header = selected
+            selected = selected.right
+        return smallest_header
+
+    def get_rows_in_column(self, column_header):
+        rows = []
+        data = column_header.down
+        while data is not column_header.header:
+            rows.append(data)
+            data = data.down
+        return rows
+
+    def row_bits_from_element(self, element):
+        """Returns the value of a row, that the given element is a member of, in a form suitable for np arrays"""
+        indices = []
+        starting_header = element.header
+        indices.append(starting_header.name)
+        element = element.right
+        column_header = element.header
+        while column_header.name != starting_header.name:
+            indices.append(column_header.name)
+            element = element.right
+            column_header = element.header
+        out = np.zeros(self.solution_dimensions[1])
+        out[indices] = 1
+        return out
 
 def generate_starting_matrix(num_elements, polyomino_size):
     result = []
@@ -76,6 +109,20 @@ def generate_starting_matrix(num_elements, polyomino_size):
         result.append(row)
     return np.asarray(result)
 
+def dancing_links_x(dll_2d, solution):
+    if dll_2d.main_header.right == dll_2d.main_header:
+        return solution
+    else:
+        next_dll_2d = copy.deepcopy(dll_2d)
+        selected_column = next_dll_2d.find_smallest_header()
+        rows_in_column = next_dll_2d.get_rows_in_column(selected_column)
+        selected_row = rows_in_column[np.random.randint(0, len(rows_in_column))]
+        solution.append(dll_2d.row_bits_from_element(selected_row))
+        # pick one at random, add it to the partial solution
+        # remove all columns satisfied by that row
+        # recurse
+
+
 def main():
     array = np.zeros((3, 3), dtype=int)
     polyomino_size = 3
@@ -84,9 +131,9 @@ def main():
     solution_matrix = generate_starting_matrix(array.size, polyomino_size)
     print("Generating initial 2D doubly linked list")
     dll_2d = DLL_2D(solution_matrix)
-    pass
-    # Recursively solve
-    # return solution.... somehow
+    print("Solving...")
+    solution = []
+    solution = dancing_links_x(dll_2d, solution)
 
 
 main()
